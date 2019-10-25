f34589d97a1ee824d9f63c1fdb25864ca0b9082f
xuan <362386736@qq.com>
2019-10-25 09:16:45 +0800

    修改内容

--- app/src/main/java/com/imoonx/kotlin/MainActivity.kt
+++ app/src/main/java/com/imoonx/kotlin/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.imoonx.kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        desc.text = getString(R.string.app_name)
+        desc.setOnClickListener {
+            Toast.makeText(this, MainActivity::class.simpleName, Toast.LENGTH_LONG).show()
+            Log.e(MainActivity::class.simpleName, MainActivity::class.simpleName ?: "123456")
+        }
     }
 }
