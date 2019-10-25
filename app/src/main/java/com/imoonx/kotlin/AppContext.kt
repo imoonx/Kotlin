6e0bd0c048b64c78d66b98b2dc7efb337e717745
xuan <362386736@qq.com>
2019-10-25 17:36:50 +0800

    修改

--- app/src/main/java/com/imoonx/kotlin/AppContext.kt
+++ app/src/main/java/com/imoonx/kotlin/AppContext.kt
@@ -1,6 +1,7 @@
 package com.imoonx.kotlin
 
 import com.imoonx.util.BaseApplication
+import com.imoonx.util.TDevice
 import com.imoonx.util.XLog
 
 /**
@@ -11,6 +12,10 @@ class AppContext : BaseApplication() {
     override fun onCreate() {
         super.onCreate()
         XLog.setIsPrint(BuildConfig.DEBUG)
+//        val log4jConfig = Log4jConfig()
+//        log4jConfig.appName = "Kotlin"
+//        log4jConfig.rootLevel = Level.ERROR
+//        log4jConfig.configure()
+        XLog.e(AppContext::class.java, "应用当前版本：" + TDevice.getVersionName())
     }
-
 }
